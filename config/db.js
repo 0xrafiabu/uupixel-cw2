96cc5b4ab5d991f9746035b3880c160e0342fd7c
Rafi Abu <0xrafiabu@MacBookAir.communityfibre.co.uk>
2026-05-07 19:25:01 +0100

    Fixed Azure SQL configuration

--- config/db.js
+++ config/db.js
@@ -1,18 +1,24 @@
 const sql = require("mssql");
 
 const config = {
-  connectionString: process.env.SQL_CONNECTION_STRING,
-  options: {
-    encrypt: true,
-    trustServerCertificate: false,
-  },
+    server: process.env.SQL_SERVER,
+    database: process.env.SQL_DATABASE,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    port: 1433,
+
+    options: {
+        encrypt: true,
+        trustServerCertificate: false
+    }
 };
 
 const pool = new sql.ConnectionPool(config);
+
 const poolConnect = pool.connect();
 
 module.exports = {
-  sql,
-  pool,
-  poolConnect,
+    sql,
+    pool,
+    poolConnect
 };
